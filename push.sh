86eb78b12b4f1c285763cb8b8613b598994dc981
yangjh-xbmu <yangjh@yeah.net>
2023-09-07 10:58:50 +0800

    自动推送

--- push.sh
+++ push.sh
@@ -1,6 +1,4 @@
-git config --global user.name 'yangjh-xbmu'
-git config --global user.email 'yangjh@xbmu.edu.cn'
-git config --global http.sslverify false
+
 git add .
 git commit -m '自动推送'
 git push
